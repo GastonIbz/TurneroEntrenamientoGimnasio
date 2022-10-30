af64cd0f2d5aba3fbe5cdcc77b82974678be4439
Gastonibanez <81576503+Gastonibanez0@users.noreply.github.com>
2022-10-30 17:17:14 -0300

    Metodos y Modal Eliminar Cancelar y Borrar - Funcionando correctamente

--- TurneroGimFuncional/Client/Services/IHttpService.cs
+++ TurneroGimFuncional/Client/Services/IHttpService.cs
@@ -2,6 +2,7 @@
 {
     public interface IHttpService
     {
+        Task<HttpRespuesta<object>> Delete(string url);
         Task<HttpRespuesta<Res>> Get<Res>(string url);
         Task<HttpRespuesta<object>> Post<Res>(string url, Res enviar);
         Task<HttpRespuesta<object>> Put<Res>(string url, Res enviar);
